4e970caf6672917795d3c3d9b6e999f556af7167
Muharrem Aslan <73404957+Maslan34@users.noreply.github.com>
2022-05-16 22:36:35 +0300

    DECRYPTION PART

--- TheCeaserCipher.c
+++ TheCeaserCipher.c
@@ -13,10 +13,143 @@ struct PlainText{
 	
 }object;
 
+int indexFinder(int,float[]);
 
 int main(int argc, char *argv[]) {
 	
+FILE *readingPtr,*writingPtr;
+	int i,j=0,index,totalLetter=0,LengthOfString,index_find,index_encoded;
+	char ExaminationText[30];
+	char ExaminationChar;
+	float temp=0.0f;
 
+	
+	char Letter_Probability[26]={'E','T','A','I','N','O','S','H','R','D','L','U','C','M','F','W','Y','G','P','B','V','K','Q','J','X','Z'};
+	//sorted by possibility of existing a character 
+	
+	float Occurrencies_Letters[26]={0}; // A to Z
+	float Sorted_Occurrencies_Letters[26];
+	
+	
+	if( (readingPtr=fopen("CipherText.txt","r"))==NULL)
+		printf("OPENING FILE ERROR!");
+	else
+	{
+		while(!feof(readingPtr))
+		{
+			fscanf(readingPtr,"%s ",object.text);
+			
+		
+			
+			strcpy(ExaminationText,object.text);
+			strcpy(ExaminationText,strupr(ExaminationText)); // getting Upper Str
+	
+			LengthOfString=strlen(ExaminationText);
+			
+			
+			for(i=0;i<LengthOfString;i++,j++){
+				
+				printf("%c",ExaminationText[i]);
+				
+				if(ExaminationText[i]>90 || ExaminationText[i]<65) 	// controlling nonalphabetic & lowercase characters.
+					continue;
+		
+		
+				index=(ExaminationText[i]%65);
+				
+				Occurrencies_Letters[index]+=1;
+				totalLetter+=1;
+			}
+			
+		}
+
+	}
+	//printf("\ntotal:%d\n",totalLetter); //->controlling
+	
+	fclose(readingPtr);
+	
+	for(i=0;i<26;i++){
+		Occurrencies_Letters[i]=(Occurrencies_Letters[i]/totalLetter)*1000; //rating
+	}
+	
+	for(i=0;i<26;i++){
+		printf("\nThe rating of %c: %f\n",i+65,Occurrencies_Letters[i]);
+	}
+	
+	
+	
+	//Copying Array Elements
+	for(i=0;i<26;i++){
+		Sorted_Occurrencies_Letters[i]=Occurrencies_Letters[i];
+	}
+	//Copying Array Elements
+	
+	
+	
+	//Sorting Array after getting rates
+	for(i=0;i<26;i++)
+		{
+		    for(j=0;j<26-i-1;j++)
+		    {
+		        if(Sorted_Occurrencies_Letters[j]<Sorted_Occurrencies_Letters[j+1])
+		        {
+		            temp=Sorted_Occurrencies_Letters[j+1];//swaping element 
+		            Sorted_Occurrencies_Letters[j+1]=Sorted_Occurrencies_Letters[j];
+		            Sorted_Occurrencies_Letters[j]=temp;
+		        }
+		    }
+		}
+		/*
+		printf("\n\nElement After Sorting \n\n");
+		for(i= 0; i< 26; i++) 
+		{
+		    printf("%.2f  ", Sorted_Occurrencies_Letters[i]); to see it
+		
+				printf("\n\n");
+	}
+*/
+
+	//DECRYPTION
+	if( ((writingPtr=fopen("Predefined-Rates.txt","w"))==NULL) || ((readingPtr=fopen("CipherText.txt","r"))==NULL))
+		printf("OPENING FILE ERROR!");
+	else
+	{
+		while(!feof(readingPtr))
+		{
+			fscanf(readingPtr,"%s ",object.text);
+		
+			
+			//printf("%s->text\n",object.text); ->cant assignt it this way
+			
+			strcpy(ExaminationText,object.text);
+			strcpy(ExaminationText,strupr(ExaminationText));// getting Upper Str
+			
+			LengthOfString=strlen(ExaminationText);
+			
+			
+			for(i=0;i<LengthOfString;i++,j++){
+				
+				if(ExaminationText[i]>90 || ExaminationText[i]<65) 	// controlling nonalphabetic & lowercase characters.
+					continue;
+					
+				index_find=(ExaminationText[i]%65);
+				printf("\nindexFind->%c\n",index_find+65);
+				
+				
+				index_encoded=indexFinder(Occurrencies_Letters[index_find],Sorted_Occurrencies_Letters);
+				
+				fprintf(writingPtr,"%c",Letter_Probability[index_encoded]);
+			}
+			fprintf(writingPtr,"%c",' ');
+			
+		}
+		
+	}
+	//END OF DECRYPTION
+	
+	
+	
+	
 
 	//CRYPTION
 	
@@ -61,7 +194,6 @@ int main(int argc, char *argv[]) {
 				fprintf(writingPtr,"%c",Alphabet[encodedChar]);
 			}
 			
-			fprintf(writingPtr,"%c",' '); // adding a space after a word
 			
 		}
 		
